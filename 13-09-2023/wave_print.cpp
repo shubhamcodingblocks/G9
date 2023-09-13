986fef0ba27929b8295cad1e86fe7392a73768bf
codeshubham <144806483+shubhamcodingblocks@users.noreply.github.com>
2023-09-13 23:19:26 +0530

    moving files

--- 13-09-2023/wave_print.cpp
+++ 13-09-2023/wave_print.cpp
@@ -2,17 +2,12 @@
 using namespace std;
 
 int main(){
-	int a[3][3] = {1,2,3,4,5,6,7,8,9};
-	cout<<"matrix: "<<endl;
-	for(int i=0;i<3;i++){
-		for(int j=0;j<3;j++){
-			cout<<a[i][j]<<" ";
-		}
-		cout<<endl;
-	}
-
-	//wave print
-	for(int col=0;col<3;col++){
+	int a[3][4] = {
+		{1,2,3,7},
+		{4,5,6,8},
+		{7,8,9,9}
+	};
+	for(int col=0;col<4;col++){
 		if(col%2==0){
 			for(int row=0;row<3;row++){
 				cout<<a[row][col]<<" ";
@@ -23,6 +18,6 @@ int main(){
 				cout<<a[row][col]<<" ";
 			}
 		}
-	}	
+	}
 	return 0;
 }
