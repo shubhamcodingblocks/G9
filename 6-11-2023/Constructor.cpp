3ad2538674e6ff3e540b6c9521224e9ceabd732f
codeshubham <144806483+shubhamcodingblocks@users.noreply.github.com>
2023-11-06 11:41:13 +0530

    constructor, destructor and copy constructor

---
+++ 6-11-2023/Constructor.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+using namespace std;
+
+class Car{
+public:
+	char *name;
+	int price;
+	int mileage;
+	int seats;
+
+	Car(char* n,int p,int m,int s){ // constructor
+		name = new char[strlen(n)+1];
+		strcpy(name,n);
+		price = p;
+		mileage = m;
+		seats = s;
+	}
+
+	// Car(Car &X){
+	// 	cout << "copy constructor called. "<<endl;
+	// 	name = new char[strlen(X.name)+1];
+	// 	strcpy(name,X.name);
+	// 	price = X.price;
+	// 	mileage = X.mileage;
+	// 	seats = X.seats;
+	// }
+
+	void display(){
+		cout << "name: " << name <<endl;
+		cout << "price: " << price <<endl;
+		cout << "mileage: " << mileage <<endl;
+		cout << "seats: " << seats <<endl;
+		cout << endl;
+	}
+
+	~Car(){ // destructor
+		delete[] name;
+		name = NULL;
+	}
+};
+
+int main(){
+	char name[] = "BMW";
+	Car C(name,100,5,4);
+	Car D = C;
+	Car E(C);
+	E.name[0] = 'A';
+	E.price = 200;
+	C.display();
+	D.display();
+	E.display();
+	return 0;
+}
